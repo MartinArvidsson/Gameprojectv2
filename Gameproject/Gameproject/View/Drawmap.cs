b07c3dfffd6d6b4d0cc190247c7ebf2f3ec3338b
Martin Arvidsson UD14 <ma223ku@student.lnu.se>
2015-12-16 19:18:24 +0100

    Textures yaaay

--- Gameproject/Gameproject/View/Drawmap.cs
+++ Gameproject/Gameproject/View/Drawmap.cs
@@ -12,7 +12,7 @@ namespace Gameproject.View
         public void Drawlevel(int [,] map,List<Texture2D> maptextures,SpriteBatch spritebatch, Camera camera)
         {
             float tilesize = camera.scaledgame();
-            float scalegame = camera.Scale(2f, tilesize);
+            float scalegame = camera.Scale(0.1f, tilesize);
             tilesize *= scalegame;
 
             for (int x = 0; x < map.GetLength(1); x++)
@@ -20,10 +20,13 @@ namespace Gameproject.View
                 for(int y = 0; y < map.GetLength(0); y++)
                 {
                     int texture = map[y, x];
-                    if(texture == -1)
-                    continue;
+                    if (texture == -1)
+                    {
+                        continue;
+                    }
                     Texture2D _texture = maptextures[texture];
-                    spritebatch.Draw(_texture, new Rectangle(x * (int)tilesize, (int)tilesize, (int)tilesize, (int)tilesize), Color.White);
+                    spritebatch.Draw(_texture, new Rectangle(x * (int)tilesize, y * (int)tilesize, (int)tilesize, (int)tilesize), Color.White);
+                    
                 }
             }
             
